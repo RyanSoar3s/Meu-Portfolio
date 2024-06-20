c5fa9d19f4f211b4fa3f3f8347fe7ba4a1736be5
Ryan Soares <ryansoares.dev@gmail.com>
2024-06-20 14:22:18 -0300

    Adição das variavéis de entrada e remoção dos métodos na classe

--- portifolio/src/app/components/zoom-photo/zoom-photo.component.ts
+++ portifolio/src/app/components/zoom-photo/zoom-photo.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-zoom-photo',
   standalone: true,
-  imports: [],
+  imports: [ NavigationComponent ],
   templateUrl: './zoom-photo.component.html',
   styleUrl: './zoom-photo.component.scss'
 })
-export class ZoomPhotoComponent {
+export class ZoomPhotoComponent{
+  @Input() path!: string;
+  @Input({
+    transform: (value: string | boolean) => (typeof value === "string") ?
+                                              value == "" || value == "true" : value
+
+  }) hidden!: boolean;
 
 }
