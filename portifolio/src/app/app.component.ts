1edf6c98fd25cb08c6eba889c1c33f6768e4bfa8
Ryan Soares <ryansoares.dev@gmail.com>
2024-06-25 12:03:42 -0300

    Adição do componente de início

--- portifolio/src/app/app.component.ts
+++ portifolio/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    NavigationComponent
+    NavigationComponent,
+    InicioComponent
+
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
