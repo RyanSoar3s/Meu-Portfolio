3d097442355f164a785530b19adfaa73838493cc
Ryan Soares <ryansoares.dev@gmail.com>
2024-07-03 11:16:29 -0300

    Componente 'meus-projetos' foi gerado

---
+++ portfolio/src/app/components/meus-projetos/meus-projetos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meus-projetos',
+  standalone: true,
+  imports: [],
+  templateUrl: './meus-projetos.component.html',
+  styleUrl: './meus-projetos.component.scss'
+})
+export class MeusProjetosComponent {
+
+}
