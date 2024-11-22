f28d9248280787b6bc53c10ea31f4a6937f58fa1
Ryan Soares <ryansoares.dev@gmail.com>
2024-11-21 21:46:05 -0300

    Animação ao carregar página

--- portfolio/src/app/components/inicio/inicio.component.ts
+++ portfolio/src/app/components/inicio/inicio.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
   imports: [],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.scss'
+  styleUrls: ['./inicio.component.scss'],
+  animations: [
+    trigger("textLoadAnimation", [
+      transition(":enter", [
+        style({
+          opacity: 0,
+          right: "150px"
+
+        }),
+        animate("2s ease-in-out", style({
+          opacity: 1,
+          right: "0px"
+
+        }))
+
+      ])
+
+    ]),
+    trigger("imgLoadAnimation", [
+        transition(":enter", [
+          style({
+            opacity: 0,
+            left: "150px"
+
+
+          }),
+          animate("2s ease-in-out", style({
+            opacity: 1,
+            left: "0px"
+
+          }))
+
+        ])
+
+    ])
+
+  ]
+
 })
 export class InicioComponent {
   path: string = "assets/foto-principal.png";
 
 }
+
