83f1f909ec7a6170366b8d9ea4a75cd38ab7128a
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-02 14:20:59 -0300

    Remoção do método que captura o evento de scroll e criação de função para animar o componente no scroll

--- portfolio/src/app/components/inicio/inicio.component.ts
+++ portfolio/src/app/components/inicio/inicio.component.ts
@@ -1,11 +1,15 @@
 import {
-  Component, ElementRef, HostListener,
-  Renderer2, ViewChild
+  Component, ElementRef,
+  Renderer2, ViewChild,
+  AfterViewInit, OnDestroy
 
 } from '@angular/core';
 import { trigger, style, transition, animate } from '@angular/animations';
+import { Subscription } from 'rxjs';
 
-import { WindowService } from '../../window.service';
+import { WindowService } from '../../services/window.service';
+import { ScrollService } from '../../services/scroll.service';
+import { AnimateScrollY } from '../../interfaces/animate-scroll';
 
 @Component({
   selector: 'app-inicio',
@@ -51,17 +55,26 @@ import { WindowService } from '../../window.service';
   ]
 
 })
-export class InicioComponent {
+export class InicioComponent implements AnimateScrollY, AfterViewInit, OnDestroy {
   @ViewChild("fixed") fixed!: ElementRef;
   @ViewChild("text")   text!: ElementRef;
   @ViewChild("img")     img!: ElementRef;
 
-  path: string = "assets/foto-principal.png";
-  lastScroll:     number = 0;
-  lastScrollText: number = 0;
-  lastScrollImg:  number = 0;
+  protected path: string = "assets/foto-principal.png";
 
-  constructor(private renderer2: Renderer2, private windowService: WindowService) {}
+  private scrollSub!: Subscription;
+  private lastScroll: number = 0;
+  private scale:      number = 0;
+
+  constructor(private renderer2: Renderer2, private windowService: WindowService, private scrollService: ScrollService) {}
+
+  ngAfterViewInit(): void {
+    this.scrollSub = this.scrollService.scrollPosition$.subscribe((scrollY) => {
+    this.animateScrollY(scrollY);
+
+    });
+
+  }
 
   onAnimationStart(): void {
     this.renderer2.setStyle(this.fixed.nativeElement, "position", "fixed");
@@ -90,40 +103,29 @@ export class InicioComponent {
 
   }
 
-  @HostListener("window:scroll")
-
-  onScroll() {
-    const scrollY: number = Number(this.windowService.nativeWindow?.scrollY);
-
+  animateScrollY(scrollY: number) {
     if (scrollY > 1649) {
       this.renderer2.removeStyle(this.fixed.nativeElement, "position");
       return;
 
     }
 
-    if (this.fixed.nativeElement.style.position === "")
-      this.renderer2.setStyle(this.fixed.nativeElement, "position", "fixed");
+    this.renderer2.setStyle(this.fixed.nativeElement, "position", "fixed");
 
+    let distScroll: number = Math.abs(this.lastScroll - scrollY) * 0.25;
 
-    let windowScroll: number = window.scrollY;
-    let distScroll:   number = Math.abs(this.lastScroll - windowScroll) * 0.25;
+    this.scale = (this.lastScroll > scrollY) ? this.scale - distScroll :
+                                                  this.scale + distScroll;
 
-    if (this.lastScroll > windowScroll) {
-      this.lastScrollText -= distScroll;
-      this.lastScrollImg  -= distScroll;
+    this.renderer2.setStyle(this.text.nativeElement, "right", `${this.scale}px`);
+    this.renderer2.setStyle(this.img.nativeElement, "left", `${this.scale}px`);
 
-    }
+    this.lastScroll = scrollY;
 
-    else {
-      this.lastScrollText += distScroll;
-      this.lastScrollImg  += distScroll;
+  }
 
-    }
-
-    this.renderer2.setStyle(this.text.nativeElement, "right", `${this.lastScrollText}px`);
-    this.renderer2.setStyle(this.img.nativeElement, "left", `${this.lastScrollImg}px`);
-
-    this.lastScroll = windowScroll;
+  ngOnDestroy(): void {
+    this.scrollSub.unsubscribe();
 
   }
 
