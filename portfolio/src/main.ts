e9f63258d369b167da57bd75534c6c331171b70d
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-05 13:40:21 -0300

    Rotas da aplicação foram configuradas

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -1,11 +1,14 @@
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideClientHydration(),
-    provideAnimations()
+    provideAnimations(),
+    provideRouter(routes)
 
   ]
 
