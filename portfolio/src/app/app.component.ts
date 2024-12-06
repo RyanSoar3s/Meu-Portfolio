67e220c1ce4762010f42281919defbcb3099b14d
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-06 10:13:46 -0300

    Configuração do scroll referente às rotas definidas na área de navegação

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -5,12 +5,16 @@ import {
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
-import { SkillsAboutMeComponent } from './components/skills-about-me/skills-about-me.component';
+import { MySkillsComponent } from './components/my-skills/my-skills.component';
 import { MyProjectsComponent } from './components/my-projects/my-projects.component';
 import { AboutThisProjectComponent } from './components/about-this-project/about-this-project.component';
 
+import { ScrollPositions } from './interfaces/scroll-positions';
+
 import { WindowService } from './services/window.service';
 import { ScrollService } from './services/scroll.service';
+
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +23,7 @@ import { ScrollService } from './services/scroll.service';
     NavigationComponent,
     HomeComponent,
     AboutMeComponent,
-    SkillsAboutMeComponent,
+    MySkillsComponent,
     MyProjectsComponent,
     AboutThisProjectComponent
 
@@ -30,9 +34,20 @@ import { ScrollService } from './services/scroll.service';
 export class AppComponent implements OnInit {
   title = 'portfolio';
 
-  constructor(private windowService: WindowService, private scrollService: ScrollService) {}
+  private scrollPositions: ScrollPositions[] = [
+    { posY: 0     },
+    { posY: 2398  },
+    { posY: 12392 },
+    { posY: 13444 },
+    { posY: 14519 }
+
+  ]
+
+  constructor(private windowService: WindowService, private scrollService: ScrollService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.router.navigate(["/"], { relativeTo: this.activatedRoute })
+
     if (this.windowService.nativeWindow) {
       history.scrollRestoration = 'manual';
       this.windowService.nativeWindow.scrollTo(0, 0);
@@ -46,6 +61,12 @@ export class AppComponent implements OnInit {
   onScroll(): void {
     const scrollY: number = Number(this.windowService.nativeWindow?.scrollY);
     this.scrollService.updateScrollPosition(scrollY);
+
+  }
+
+  enableNavigationScrolling(key: number): void {
+    const posY = Number(this.scrollPositions[key].posY);
+    this.windowService.nativeWindow?.scrollTo({ top: posY, behavior: "smooth" });
 
   }
 
