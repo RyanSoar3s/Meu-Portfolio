e89a2b1ebd21a43855605dabeca5aacbb1463983
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-09 10:27:20 -0300

    Rotas foram re-configuradas

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
-import { MySkillsComponent } from './components/my-skills/my-skills.component';
-import { MyProjectsComponent } from './components/my-projects/my-projects.component';
-import { AboutThisProjectComponent } from './components/about-this-project/about-this-project.component';
+import { FormComponent } from './components/form/form.component';
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about-me", component: AboutMeComponent },
-  { path: "skills", component: MySkillsComponent },
-  { path: "my-projects", component: MyProjectsComponent },
-  { path: "about-this-project", component: AboutThisProjectComponent }
+  { path: "entre-em-contato", component: FormComponent }
 
 ];
