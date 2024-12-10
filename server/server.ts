917ad7f250ec34e4c9a7cbc9c55d4effc7243d93
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-09 21:32:54 -0300

    Arquivo do servidor foi configurado para receber os valores do formulário de e-mail

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,71 @@
 import express from 'express'
 import cors from 'cors';
+import nodemailer from 'nodemailer'
+
+import dotenv from 'dotenv';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+import { OAuth2Client } from 'google-auth-library';
+
+dotenv.config();
+
+const oAuth2Client = new OAuth2Client(
+    String(process.env.OAUTH_CLIENT_ID),
+    String(process.env.OAUTH_CLIENT_SECRET),
+    "https://developers.google.com/oauthplayground"
+)
+
+oAuth2Client.setCredentials({ refresh_token: String(process.env.OAUTH_REFRESH_TOKEN) });
 
 const app = express();
+
 app.use(cors({
     credentials: true,
     origin: [ "http://localhost:4200" ]
 
 }));
 
-app.get("/api/send-email", (req, res) => {
-    res.send("Hello");
 
-})
+app.use(express.json());
 
-const port = 5000;
+app.post("/api/send-email", async (req, res) => {
+    const { name, email, message } = req.body;
+    const token = await oAuth2Client.getAccessToken();
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+
+        auth: {
+            type: "OAuth2",
+            user: process.env.EMAIL_USER,
+            clientId: process.env.OAUTH_CLIENT_ID,
+            clientSecret: process.env.OAUTH_CLIENT_SECRET,
+            refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+            accessToken: token.token
+        },
+        logger: true,
+        debug: true
+
+    } as SMTPTransport.Options);
+    const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER,
+        subject: 'Formulário de Contato: ' + name,
+        text: `Testando envio de email - ${message}`
+    
+    };
+
+    transporter.sendMail(mailOptions, (error) => {
+        if (error) {
+          return res.status(500).send(`error: ${error}`);
+
+        }
+        res.status(200).send('E-mail enviado com sucesso');
+
+      });
+});
+
+const port = Number(process.env.HOST_PORT);
 
 app.listen(port, () => {
     console.log(`server running on port: ${port}`);
