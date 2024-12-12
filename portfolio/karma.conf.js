48577b1f15bc6e3b9089073b502b1118f00cfadb
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-12 11:31:09 -0300

    Configuração do karma.config.js

--- portfolio/karma.conf.js
+++ portfolio/karma.conf.js
@@ -7,17 +7,15 @@ module.exports = (config) => {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('karma-spec-reporter'), // Add karma-spec-reporter here
+      require('karma-spec-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-      },
+      jasmine: {},
       captureConsole: true
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/portfolio'),
@@ -27,7 +25,6 @@ module.exports = (config) => {
         { type: 'text-summary' }
       ]
     },
-    // Add 'spec' reporter to the reporters array
     reporters: ['progress', 'kjhtml', 'spec'],
     browsers: ['Chrome'],
     restartOnFileChange: true
