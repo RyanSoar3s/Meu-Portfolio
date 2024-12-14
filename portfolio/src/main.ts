dcf81f328b27687d22dd039eec9eda1cfdccc400
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-14 11:03:30 -0300

    Configuração no provider HttpClient

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -3,14 +3,14 @@ import { bootstrapApplication, provideClientHydration } from '@angular/platform-
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideClientHydration(),
     provideAnimations(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptorsFromDi())
 
   ]
 
