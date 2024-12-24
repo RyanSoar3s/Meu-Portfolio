0da93fd21bd596f0e01dfb81bb3fa63c3e61a2a6
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-23 22:55:42 -0300

    Criação da interface das propriedades do componente sobre-mim

--- portfolio/src/app/interfaces/components-property-values.d.ts
+++ portfolio/src/app/interfaces/components-property-values.d.ts
@@ -1,10 +1,12 @@
 import { AppPropertyValues } from './app-property-values';
 import { NavigationPropertyValues } from './navigation-property-values';
 import { HomePropertyValues } from './home-property-values';
+import { AboutMePropertyValues } from './about-me-property-values';
 
 export declare interface ComponentPropertyValues {
   appPropertyValues: AppPropertyValues | {},
   navigationPropertyValues: NavigationPropertyValues | {},
-  homePropertyValues: HomePropertyValues | {}
+  homePropertyValues: HomePropertyValues | {},
+  aboutMePropertyValues: AboutMePropertyValues | {}
 
 }
