89e9825e7f77b35d6f2483005915c0d985e5c94d
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-24 12:44:36 -0300

    Criação da interface do my-skills component

--- portfolio/src/app/interfaces/components-property-values.d.ts
+++ portfolio/src/app/interfaces/components-property-values.d.ts
@@ -2,11 +2,13 @@ import { AppPropertyValues } from './app-property-values';
 import { NavigationPropertyValues } from './navigation-property-values';
 import { HomePropertyValues } from './home-property-values';
 import { AboutMePropertyValues } from './about-me-property-values';
+import { MySkillsPropertyValues } from './my-skills-property-values'
 
 export declare interface ComponentPropertyValues {
   appPropertyValues: AppPropertyValues | {},
   navigationPropertyValues: NavigationPropertyValues | {},
   homePropertyValues: HomePropertyValues | {},
-  aboutMePropertyValues: AboutMePropertyValues | {}
+  aboutMePropertyValues: AboutMePropertyValues | {},
+  mySkillsPropertyValues: MySkillsPropertyValues | {}
 
 }
