ec443aa21b043251127852f4b5b55e926d545179
Ryan Soares <ryansoares.dev@gmail.com>
2024-12-28 12:55:44 -0300

    Definição das propriedades de my-projects

--- portfolio/src/app/interfaces/my-projects-property-values.d.ts
+++ portfolio/src/app/interfaces/my-projects-property-values.d.ts
@@ -1,14 +1,9 @@
 export declare interface MyProjectsPropertyValues {
-  "--my-projects-section-width": string,
-  "--my-projects-section-container-arrow-left-width": string,
-  "--my-projects-section-container-arrow-right-width": string,
-  "--my-projects-section-grid-meus-projetos-height": string,
-  "--my-projects-section-grid-meus-projetos-width": string,
-  "--my-projects-section-grid-meus-projetos-column-gap": string,
-  "--my-projects-section-grid-meus-projetos-projeto-height": string,
-  "--my-projects-section-grid-meus-projetos-projeto-width": string,
-  "--my-projects-section-grid-meus-projetos-projeto-links_externos-top": string,
-  "--my-projects-section-grid-meus-projetos-projeto-links_externos-button-height": string,
-  "--my-projects-section-grid-meus-projetos-projeto-links_externos-button-width": string
+  "--my-projects-section-margin-left": string,
+  "--my-projects-section-margin-left": string,
+  "--my-projects-section-container-grid-width": string,
+  "--my-projects-section-projeto-height": string,
+  "--my-projects-section-projeto-width": string,
+  "--my-projects-section-buttons-gap": string
 
 }
