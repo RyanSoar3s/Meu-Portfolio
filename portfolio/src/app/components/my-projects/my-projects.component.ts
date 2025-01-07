1ff628b5377ce090396e6da2e8eb57ad5801c601
Ryan Soares <ryansoares.dev@gmail.com>
2025-01-07 13:25:49 -0300

    Componente refatorado

--- portfolio/src/app/components/my-projects/my-projects.component.ts
+++ portfolio/src/app/components/my-projects/my-projects.component.ts
@@ -1,6 +1,7 @@
 import {
   Component, ElementRef, OnInit,
-  Renderer2, ViewChild
+  Renderer2, ViewChild, ViewChildren,
+  QueryList
 
 } from '@angular/core';
 import { ResponsiveObservableService } from '../../services/responsive-observable.service';
@@ -26,8 +27,9 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
   styleUrl: './my-projects.component.scss'
 })
 
-export class MyProjectsComponent implements OnInit{
+export class MyProjectsComponent implements OnInit {
   @ViewChild("grid") grid!: ElementRef;
+  @ViewChildren("projects") projects!: QueryList<ElementRef>;
 
   protected readonly path_projeto_vazio: string = "../../../assets/projeto-vazio.png";
   protected readonly path_projeto_1:     string = "../../../assets/projeto-1.png";
@@ -40,7 +42,11 @@ export class MyProjectsComponent implements OnInit{
   private direction: number = 0;
   private data_id: number = 2;
 
-  constructor(private renderer: Renderer2, private responsiveObservableService: ResponsiveObservableService) {}
+  constructor(
+    private renderer: Renderer2,
+    private responsiveObservableService: ResponsiveObservableService
+
+  ) {}
 
   ngOnInit(): void {
     this.responsiveObservableService.observe("my-projects");
@@ -64,12 +70,37 @@ export class MyProjectsComponent implements OnInit{
     else return;
 
     let aux_element = document.querySelector(`[data-project='${data_id_aux}']`) as HTMLElement;
-    aux_element.classList.remove("projeto-principal");
+    let child = aux_element.children[0];
+    this.renderer.removeClass(aux_element, "projeto-principal");
+    this.renderer.removeClass(child, "hidden");
 
     let cur_element = document.querySelector(`[data-project='${this.data_id}']`) as HTMLElement;
-    cur_element.classList.add("projeto-principal");
 
-    this.renderer.setStyle(this.grid.nativeElement, "left", `${this.direction}vw`)
+    this.renderer.addClass(cur_element, "projeto-principal");
+
+    this.renderer.setStyle(this.grid.nativeElement, "transform", `translateX(${this.direction}vw)`);
+
+  }
+
+  onClick(): void {
+    this.projects.forEach((element: ElementRef) => {
+      let child = element.nativeElement.children[0];
+
+      if (element.nativeElement.classList.contains("projeto-principal")) {
+        if (!child.classList.contains("hidden"))
+          this.renderer.addClass(child, "hidden");
+
+        else
+          this.renderer.removeClass(child, "hidden");
+
+      }
+
+      else {
+        this.renderer.removeClass(child, "hidden");
+
+      }
+
+    });
 
   }
 
