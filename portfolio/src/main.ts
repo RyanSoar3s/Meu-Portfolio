b025ab440211f440f3b258920e9993ea68fc46ad
Ryan Soares <ryansoares.dev@gmail.com>
2025-01-07 13:35:31 -0300

    Ajuste nos providers

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -1,16 +1,11 @@
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideClientHydration(),
-    provideAnimations(),
-    provideRouter(routes),
-    provideHttpClient()
+    provideAnimations()
 
   ]
 
